c10530131b8091c9237bd1a773af36649a227eb8
ThilinaDananjaya <66713156+ThilinaDananjaya@users.noreply.github.com>
2021-06-23 18:30:36 +0530

    sign in

---
+++ frontend/src/components/SignIn/signin.component.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import "./signin.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default class SignIn extends Component {
+  state = {
+    email: "",
+    password: "",
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      change: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <div className="div-signin">
+        <div className="div-signin-logo">
+          <Logo />
+        </div>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              required
+              onChange={this.handleChange}
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              required
+              onChange={this.handleChange}
+            />
+            <button onSubmit={this.handleSubmit}>Sign In</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}

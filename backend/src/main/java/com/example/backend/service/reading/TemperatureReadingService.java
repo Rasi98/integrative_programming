015f08f76468bb845a6412e506e37baedecc9157
Gothama Nayaneetha Rajawasam <49790149+Gothama@users.noreply.github.com>
2021-06-30 15:43:42 +0530

    Developed Reading and Sensor endpoints

---
+++ backend/src/main/java/com/example/backend/service/reading/TemperatureReadingService.java
@@ -0,0 +1,24 @@
+package com.example.backend.service.reading;
+
+
+import com.example.backend.entity.reading.TemperatureReading;
+import com.example.backend.repository.reading.TemperatureReadingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TemperatureReadingService implements TemperatureReadingServiceInterface {
+
+    @Autowired
+    private TemperatureReadingRepository temperatureReadingRepository;
+
+    @Override
+    public TemperatureReading saveTemperatureReading(TemperatureReading temperatureReading){
+        System.out.println(temperatureReading.getTemperaturevalue());
+        return temperatureReadingRepository.save(temperatureReading);
+    }
+
+
+
+
+}

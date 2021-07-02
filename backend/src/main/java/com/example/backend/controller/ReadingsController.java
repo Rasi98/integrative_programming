cc47f661335d9f3cc07eff6b2b32868abd31121c
Gothama Nayaneetha Rajawasam <49790149+Gothama@users.noreply.github.com>
2021-07-02 21:26:44 +0530

    Notification channels and manage alerts added. CORS fixed.

--- backend/src/main/java/com/example/backend/controller/ReadingsController.java
+++ backend/src/main/java/com/example/backend/controller/ReadingsController.java
@@ -3,13 +3,11 @@ package com.example.backend.controller;
 import com.example.backend.entity.reading.TemperatureReading;
 import com.example.backend.service.reading.TemperatureReadingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/temperaturereading")
 public class ReadingsController {
--- backend/src/main/java/com/example/backend/service/reading/TemperatureReadingService.java
+++ backend/src/main/java/com/example/backend/service/reading/TemperatureReadingService.java
@@ -1,6 +1,10 @@
 package com.example.backend.service.reading;
 
 
+import com.example.backend.ManageAlerts.AlertObserver;
+import com.example.backend.ManageAlerts.AlertObserverCall;
+import com.example.backend.ManageAlerts.AlertObserverEmail;
+import com.example.backend.ManageAlerts.AlertSubject;
 import com.example.backend.entity.reading.TemperatureReading;
 import com.example.backend.repository.reading.TemperatureReadingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +20,11 @@ public class TemperatureReadingService implements TemperatureReadingServiceInter
     public TemperatureReading saveTemperatureReading(TemperatureReading temperatureReading){
        // System.out.println(temperatureReading.getTemperaturevalue());
       //  temperatureReading.setAlert(true);
+        AlertSubject alertSubject = new AlertSubject();
+        AlertObserver k1 = new AlertObserverCall(alertSubject , "0183848388");
+        AlertObserver k2 = new AlertObserverEmail(alertSubject, "uddf@gmail.com");
+        AlertObserver k3 = new AlertObserverEmail(alertSubject, "gothama@gmail.com");
+        alertSubject.setSensorid("89");
         return temperatureReadingRepository.save(temperatureReading);
     }
 

c0ad5113b6d597ce98bfe829d843f749b742b54d
Gothama Nayaneetha Rajawasam <49790149+Gothama@users.noreply.github.com>
2021-07-06 01:20:32 +0530

    CORS fixed

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -2,9 +2,7 @@ package com.example.backend.controller;
 
 import com.example.backend.service.user.StaffUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController

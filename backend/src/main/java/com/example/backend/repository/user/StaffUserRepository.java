af8ac5037619be7c126a588124ab2aa17c109e11
prabasarim17055 <prabasari1996@gmail.com>
2021-07-06 01:02:05 +0530

    added login - backend
    User and staff user login

--- backend/src/main/java/com/example/backend/repository/user/StaffUserRepository.java
+++ backend/src/main/java/com/example/backend/repository/user/StaffUserRepository.java
@@ -1,8 +1,9 @@
 package com.example.backend.repository.user;
 
+import com.example.backend.entity.user.StaffUser;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StaffUserRepository extends UserRepository {
-
+    StaffUser findByUsernameAndPassword(String username, String password);
 }

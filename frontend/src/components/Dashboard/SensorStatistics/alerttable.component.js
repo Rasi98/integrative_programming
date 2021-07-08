ed9ce50a60a9f1629f0358288d6309f09b1a03ac
Gothama Nayaneetha Rajawasam <49790149+Gothama@users.noreply.github.com>
2021-07-08 22:08:51 +0530

    Added

--- frontend/src/components/Dashboard/SensorStatistics/alerttable.component.js
+++ frontend/src/components/Dashboard/SensorStatistics/alerttable.component.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react'
 import { Table, Card } from 'react-bootstrap';
+import Moment from 'react-moment';
+
+
+import axios from 'axios';
+
+const siAPI1= axios.create({
+    baseURL :`http://localhost:9090/sensor/getalertsofsensor`
+  })
+
 
 export default class AlertTable extends Component {
+
+constructor(props){
+  super(props);
+  console.log("Alert" + this.props.sensorID)
+  this.getAllSensorsReadings();
+}
+
+state={
+  sensorReadings:[]
+}
+
+getAllSensorsReadings=()=>{
+  siAPI1.get(`/1`).then(res=>{
+      console.log(res.data)
+      this.setState({
+        sensorReadings:res.data
+      })
+  }).catch(err => {
+   window.alert(err)
+})
+}
+
+
     render() {
         return (
           <div style={{marginTop:"20px",width:"640px", padding: "18px"}}>
@@ -18,30 +50,15 @@ export default class AlertTable extends Component {
     </tr>
   </thead>
   <tbody>
+  {this.state.sensorReadings.map(r=>
     <tr>
       <td>1</td>
-      <td>2010/09/90</td>
-      <td>19.00H</td>
-      <td>67C</td>
+      <td><Moment format="YYYY/MM/DD">{r.date}</Moment></td>
+      <td><Moment format="hh:mm:ss">{r.date}</Moment></td>
+      <td>{r.temperaturevalue}</td>
     </tr>
-    <tr>
-    <td>1</td>
-      <td>2010/09/90</td>
-      <td>19.00H</td>
-      <td>67C</td>
-    </tr>
-    <tr>
-    <td>1</td>
-      <td>2010/09/90</td>
-      <td>19.00H</td>
-      <td>67C</td>
-    </tr>
-    <tr>
-    <td>1</td>
-      <td>2010/09/90</td>
-      <td>19.00H</td>
-      <td>67C</td>
-    </tr>
+  )}
+    
    
   </tbody>
 </Table>
